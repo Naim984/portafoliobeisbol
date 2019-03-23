30e4d6637db4f7ad5f5a888c9375aad890cab704
Denier Naim Ferrer Rodriguez <dferrerr@everis.com>
2019-03-23 13:46:29 +0100

    fin seccion 4

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Portafolio
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { InfoPaginaService } from './services/info-pagina.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portafolio';
+
+  constructor( public infoPaginaService: InfoPaginaService ) {
+
+  }
+
 }
